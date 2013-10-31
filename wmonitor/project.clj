4b33c7ca007e15ca4cf9697b54a410ca0bc61d7a
fhitchen <fhitchen@gmail.com>
2013-10-31 15:53:15 -0500

    Initial Commit

---
+++ wmonitor/project.clj
@@ -0,0 +1,11 @@
+(defproject woopramonitor "0.1.0-SNAPSHOT"
+  :description "Simple monitor for AIO Woopra"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.7.7"]
+                 [org.clojure/data.json "0.2.3"]]
+  :main woopramonitor.queries
+  :aot  [woopramonitor.queries])
+  
