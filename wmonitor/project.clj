a139c25007d6bdef606b1b34e4b1ade3be159c1a
fhitchen <fhitchen@gmail.com>
2013-10-31 18:13:44 -0500

    asdf

--- wmonitor/project.clj
+++ wmonitor/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
-                 [org.clojure/data.json "0.2.3"]]
+                 [org.clojure/data.json "0.2.4"]]
   :main woopramonitor.queries
   :aot  [woopramonitor.queries])
   
