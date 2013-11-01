4405f77f6584808db4af470d361b71c46b80024b
fhitchen <fhitchen@gmail.com>
2013-10-31 19:04:26 -0500

    Small mistake to do with changed NS.

--- wmonitor/project.clj
+++ wmonitor/project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.7.7"]
                  [org.clojure/data.json "0.2.3"]]
-  :main woopramonitor.queries
-  :aot  [woopramonitor.queries])
+  :main wmonitor.queries
+  :aot  [wmonitor.queries])
   
